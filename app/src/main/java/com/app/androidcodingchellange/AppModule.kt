6634c8d1ff10bb5f448d1ad51ebc82a6cc54d1d1
Asadullah Mumtaz <asadullah.mumtaz@attech-ltd.com>
2021-10-23 10:06:24 +0500

    - Required configuration completed for the dependency injection

---
+++ app/src/main/java/com/app/androidcodingchellange/AppModule.kt
@@ -0,0 +1,16 @@
+package com.app.androidcodingchellange
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideTestString() = "This is a string we will inject"
+}
---
+++ app/src/main/java/com/app/androidcodingchellange/CustomApplication.kt
@@ -0,0 +1,10 @@
+package com.app.androidcodingchellange
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CustomApplication : Application() {
+
+
+}
